0cb357c89fd1198188ce014a4d265a16053317dd
uriahaleshire24 <uriahaleshire24@Mac.local>
2019-09-18 18:36:03 -0500

    another one1

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,60 @@
 //start
-const word = ["lionking", "aladdin", "perterpan", 
-"frozen", "liloandstitch", "bambi", 
-"robinhood", "atlantis", "theemperorsnewgroove",
-"treasureplanet", "therescuedownunder", "holes"];
+var word = ["cars", "aladdin", "walle", "up", "ratatouille", 
+"frozen", "hercules", "bambi", "lionking", "brave",  
+"cinderella", "atlantis", "zootopia", "abugslife", "atlantis",
+"pinocchio", "pocahontas", "holes", "avengers", "brotherbear"];
 console.log(word)
 
-var randNum = Math.floor(Math.random() * word.length);
-var chosenWord = word[randNum];
+var chosenWord = "";
+
 var rightWord = [];
 var wrongWord = [];
 var underScore =[];
 /////
-var docUnderScore = document.getElementsByClassName("underscore");
-var docRightGuess = document.getElementsByClassName("rightGuess");
-var docWrongGuess = document.getElementsByClassName("wrongGuess");
+var docUnderScore = document.getElementById("underscore");
+var docRightGuess = document.getElementById("rightGuess");
+var docWrongGuess = document.getElementById("wrongGuess");
 
 ////////
 
+findWord();
+
 console.log(chosenWord);
 
-var generateUnderscore = () => {
-    for(var i = 0; i < chosenWord.length; i++) {
-        underScore.push('_');
+function generateUnderscore() {
+    if(underScore.length === 0){
+        for(var i = 0; i < chosenWord.length; i++) {
+            underScore.push(' _ ');
+            console.log("pushed: " + i);
+        }
     }
-    return underScore;
+
+    
+    docUnderScore.textContent = underScore.join("");
+
+    // return underScore
 }
-console.log(generateUnderscore());
+//console.log(generateUnderscore());
+
+// generateUnderscore();
+// generateUnderscore();
+
+//This function finds a new word
+function findWord(){
+    var randNum = Math.floor(Math.random() * word.length);
+    chosenWord = word[randNum];
+
+    generateUnderscore();
+}
 
 document.addEventListener("keypress", (event) => {
+    console.log(event.key)
     var keyword = String.fromCharCode(event.keyCode);
     
         if(chosenWord.indexOf(keyword) > -1) {
             rightWord.push(keyword);
             
-
+            
             underScore[chosenWord.indexOf(keyword)] = keyword;
             docUnderScore[0].innerHTML = underScore.join(' ');
             // docRightGuess[0].innerHTML = rightWord;
@@ -47,4 +68,4 @@ document.addEventListener("keypress", (event) => {
         } 
 
 }); 
-docUnderScore[0].innerHTML = generateUnderscore().join(' ');
+//docUnderScore.textContent = generateUnderscore();
